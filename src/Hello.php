f7256fc2203a16fb5ad0acfe514937bc8028095b
lokeshsolanki <lokeshsolanki0206@gmail.com>
2020-07-12 19:14:34 +0530

    Update Hello.php

--- src/Hello.php
+++ src/Hello.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Psi\Libraries\HelloWorldProgramByLsolankinew\CExample;
+use Psi\Libraries\HelloWorldProgramByLsolanki\CExample;
 
 $instance = new CExample();
 
