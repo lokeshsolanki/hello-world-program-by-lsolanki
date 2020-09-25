21310da87ebec863c7293f551f910d5a3cb32135
lokeshsolanki <lokeshsolanki0206@gmail.com>
2020-09-25 12:13:19 +0530

    Delete Hello.php

--- src/Hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Psi\Libraries\HelloWorldProgramByLsolanki\CExample;
-
-$instance = new CExample();
-
-echo $instance->hello("Hello World!");
